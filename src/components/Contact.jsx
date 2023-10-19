b06455d998f82d590f1d4baf7a92b37bf3a6b005
NIKHIL NAILWAL <78683932+nailwal08@users.noreply.github.com>
2023-10-19 12:02:49 +0530

    Create Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <>
+      <div className="contactportion">
+        <a href="https://github.com/nailwal08" target='_blank' rel="noopener noreferrer">GITHUB</a>
+        <a href="mailto:nailwalnikhil17t@gmail.com" target='_blank' rel="noopener noreferrer">GMAIL</a>
+        <a href="https://in.linkedin.com/in/nailwalnikhil" target='_blank' rel="noopener noreferrer">LINKEDIN</a>
+      </div>
+    </>
+  );
+  
+}
+
+export default Contact;
+
